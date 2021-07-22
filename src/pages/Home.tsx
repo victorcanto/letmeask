48225bdd889a7b1c2d4c9c6473577d8cf62cf028
Victor Canto <iamvictorcanto@gmail.com>
2021-07-22 01:27:38 -0300

    styles: Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,9 +2,11 @@ import illustrationImage from '../assets/images/illustration.svg';
 import logoImage from '../assets/images/logo.svg';
 import googleIcon from '../assets/images/google-icon.svg';
 
+import '../styles/auth.scss';
+
 export function Home() {
   return (
-    <div>
+    <div id='page-auth'>
       <aside>
         <img
           src={illustrationImage}
@@ -13,18 +15,20 @@ export function Home() {
         <strong>Crie salas de Q&amp;A ao vivo</strong>
         <p>Tire as dúvidas da sua audiência em tempo real</p>
       </aside>
-      <div>
-        <img src={logoImage} alt='Logo Letmeask' />
-        <button>
-          <img src={googleIcon} alt='Icone do Google' />
-          Crie uma sala com o Google
-        </button>
-        <div>ou entre em uma sala</div>
-        <form>
-          <input type='text' placeholder='Digite o código da sala' />
-          <button type='submit'>Entrar na sala</button>
-        </form>
-      </div>
+      <main>
+        <div className='main-content'>
+          <img src={logoImage} alt='Logo Letmeask' />
+          <button className='create-room'>
+            <img src={googleIcon} alt='Icone do Google' />
+            Crie uma sala com o Google
+          </button>
+          <div className='separator'>ou entre em uma sala</div>
+          <form>
+            <input type='text' placeholder='Digite o código da sala' />
+            <button type='submit'>Entrar na sala</button>
+          </form>
+        </div>
+      </main>
     </div>
   );
 }
