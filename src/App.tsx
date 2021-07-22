96e4a94fb138854ec559782507ad173f1955a148
Victor Canto <iamvictorcanto@gmail.com>
2021-07-22 03:25:03 -0300

    feat: create AuthProvider

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { NewRoom } from './pages/NewRoom';
+import { AuthProvider } from './contexts/AuthProvider';
 
 function App() {
   return (
     <Router>
-      <Route exact path='/' component={Home} />
-      <Route path='/rooms/new' component={NewRoom} />
+      <AuthProvider>
+        <Route exact path='/' component={Home} />
+        <Route path='/rooms/new' component={NewRoom} />
+      </AuthProvider>
     </Router>
   );
 }
---
+++ src/contexts/AuthProvider.tsx
@@ -0,0 +1,51 @@
+import { useState, createContext } from 'react';
+import { auth, firebase } from '../services/firebase';
+
+type User = {
+  id: string;
+  name: string;
+  avatar: string;
+};
+
+type AuthContextType = {
+  user: User | undefined;
+  signInWithGoogle: () => Promise<void>;
+};
+
+type PropChildren = {
+  children: Object;
+};
+
+export const AuthContext = createContext({} as AuthContextType);
+
+export function AuthProvider(props: PropChildren) {
+  const [user, setUser] = useState<User>();
+
+  async function signInWithGoogle() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    const res = await auth.signInWithPopup(provider);
+    if (res.user) {
+      const { displayName, photoURL, uid } = res.user;
+
+      if (!displayName || !photoURL) {
+        throw new Error('Missing information from Google Account');
+      }
+      setUser({
+        id: uid,
+        name: displayName,
+        avatar: photoURL,
+      });
+    }
+  }
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        signInWithGoogle,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+}
